498184b9d4a6719c13fa685ef18e3d6791bf8c57
Jhenna Foronda <jfcaldetera@gmail.com>
2018-06-26 18:12:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name)
 {
-  return ("Wecome, " + name + ". You are number " + (katzDeliLine.indexOf(name)) + " in line.")
+  return ("Wecome, " + name + ". You are number " + (katzDeliLine.indexOf(name), 0) + " in line.")
   // return position in line
 }
