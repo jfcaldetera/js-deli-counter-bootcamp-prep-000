ab254ae8c4a9255e509c71c4b502b2ef0bcf6443
Jhenna Foronda <jfcaldetera@gmail.com>
2018-06-26 18:44:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ function currentLine(katzDeliLine)
       line += `${i + 1}. ${katzDeliLine[i]}, `
     }
 
-    return (("The line is currently: " + line).splice(0,-1))
+    return (("The line is currently: " + line).slice(0,-1))
   }
 }
