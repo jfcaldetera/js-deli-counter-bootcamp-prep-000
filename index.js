c92c91dbf3893a5e3b78f80d110a34c604c474dd
Jhenna Foronda <jfcaldetera@gmail.com>
2018-06-26 18:24:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function takeANumber(katzDeliLine, name)
   katzDeliLine.push(name)
   return(`Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`)
 }
+
+function nowServing(katzDeliLine)
+{
+  if (katzDeliLine.length < 1)
+  {
+    return "There is nobody waiting to be served!"
+  }
+  else
+  {
+    var name = katzDeliLine.shift()
+    return "Currently serving ${name}."
+  }
+
+
+}
