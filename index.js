10b5c84120f5ee9f2cd9772eda415abe509ed4ca
Jhenna Foronda <jfcaldetera@gmail.com>
2018-06-26 18:15:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name)
 {
   katzDeli.push('${name}')
-  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
 }
